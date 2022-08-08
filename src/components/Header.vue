e15f92e41afad1d0dc2cbcebbff6b3a5113c7fc9
Yutaka Catch <yyycatch@gmail.com>
2022-08-08 16:47:29 +0900

    fork from hexa-vue-example1

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-app-bar color="light-blue accent-4" dense dark>
+      <v-toolbar-title>hexa ToDo Example</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <AvatarMenu />
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import AvatarMenu from "@/components/AvatarMenu";
+export default {
+  name: "Header",
+  components: {
+    AvatarMenu,
+  },
+};
+</script>

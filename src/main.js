2b65a41040f6af38dbbaad14a66bff197a4bafdf
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2022-08-09 10:43:23 +0900

    Delete HTTP service

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
 import vuetify from "@/plugins/vuetify";
-import interceptorsSetup from "@/services/http/interceptors";
+// import interceptorsSetup from "@/services/http/interceptors";
 
-interceptorsSetup();
+// interceptorsSetup();
 
 Vue.config.productionTip = false;
 
